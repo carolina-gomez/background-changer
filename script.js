72724eca0e64ff14b0a29d9e5df163de430e37f2
Carolina Gomez <carolinagomezTX@gmail.com>
2021-11-07 12:25:08 -0600

    end

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
- 
+ const btn = document.getElementById("btn")
+
+ function randomColor() {
+    return `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
+}
+
+ btn.addEventListener("click", () => {
+    document.body.style.background = randomColor();
+ });
